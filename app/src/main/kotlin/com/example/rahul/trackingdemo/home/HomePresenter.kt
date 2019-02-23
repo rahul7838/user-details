267333847c76d55a43e765ecc55be901e5b56960
rahul7838 <rahulsinghal0308@gmail.com>
2019-02-23 20:36:57 +0530

    Initial commit

--- app/src/main/kotlin/com/example/rahul/trackingdemo/home/HomePresenter.kt
+++ app/src/main/kotlin/com/example/rahul/trackingdemo/home/HomePresenter.kt
@@ -3,9 +3,11 @@ package com.example.rahul.trackingdemo.home
 import android.util.Log
 import com.example.rahul.trackingdemo.data.AppDataManager
 import com.example.rahul.trackingdemo.data.model.Result
+import com.example.rahul.trackingdemo.ui.home.HomeContract
 import com.example.rahul.trackingdemo.utils.FormatUtility
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.schedulers.Schedulers
+
 
 class HomePresenter(val appDataManager: AppDataManager) : HomeContract.Presenter {
 
@@ -38,7 +40,7 @@ class HomePresenter(val appDataManager: AppDataManager) : HomeContract.Presenter
     }
 
     override fun sortByName() {
-        listArray.sortBy { it.name }
+        listArray.sortBy{ it.name }
         view?.updateUi(listArray)
     }
 
